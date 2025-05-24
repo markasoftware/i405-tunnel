f400a10e2eb5d400754ac6a96421633a63b53ab1
Mark Polyakov <mark@markasoftware.com>
2025-05-23 21:43:35 -0700

    jumbo packets

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,4 @@
+#[cfg(not(feature = "jumbo_packets"))]
 pub(crate) const MAX_IP_PACKET_LENGTH: usize = 1472; // TODO may want to remove this to support jumbo frames?
+#[cfg(feature = "jumbo_packets")]
+pub(crate) const MAX_IP_PACKET_LENGTH: usize = 8972;
